152383b317c629ed314ca9ceab71f00a30d5145e
Aloka Disanayaka <alokamuthumani5@gmail.com>
2026-03-14 22:08:23 +0000

    Add FoodList model, controller, and migration for food_lists table

---
+++ app/Http/Controllers/FoodListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodList;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FoodListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FoodList $foodList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FoodList $foodList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FoodList $foodList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FoodList $foodList)
+    {
+        //
+    }
+}
