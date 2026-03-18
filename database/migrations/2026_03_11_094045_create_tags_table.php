4ea17d933c10921d7d0f20cd8553a5365e6fd1cb
Adhira <D00277397@student.dkit.ie>
2026-03-18 14:14:32 +0000

    added migration table files for database

--- database/migrations/2026_03_11_094045_create_tags_table.php
+++ database/migrations/2026_03_11_094045_create_tags_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('food_lists', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) 
+        {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title');
-            $table->text('description');
-            $table->timestamps();
+            $table->string('name')->unique();
         });
     }
 
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('food_lists');
+        Schema::dropIfExists('tags');
     }
 };
