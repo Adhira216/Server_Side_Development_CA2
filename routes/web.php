54dbab402f21d40a471b32881e470616e9ce1a57
Adhira <D00277397@student.dkit.ie>
2026-03-18 13:55:01 +0000

    updated web.php to have register and login routes

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,23 @@ Route::resource('lists', \App\Http\Controllers\FoodListController::class)->middl
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('lists.index');
 });
+
+use App\Http\Controllers\AuthController;
+
+//register routes
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+//login routes
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+
+Route::get('/dashboard', function () 
+{
+    return view('dashboard');
+})->middleware('auth');
