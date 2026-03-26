413450bf32784b0d9218d2c0e1e90f78f9ed5f2b
Adhira <D00277397@student.dkit.ie>
2026-03-26 12:24:03 +0000

    created sidebar component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="{{ asset('css/variables.css') }}">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+<aside class="sidebar">
+    <h2 class="sidebar-title">Browse Lists</h2>
+
+    <nav class="sidebar-nav">
+        <a href="#">All Lists</a>
+        <a href="#">Popular</a>
+    </nav>
+</aside>
