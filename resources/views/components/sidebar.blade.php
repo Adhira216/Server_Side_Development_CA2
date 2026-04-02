6ea46365979f7bae116c04ebf3c92fa8904e8ff3
Adhira <D00277397@student.dkit.ie>
2026-04-02 18:03:36 +0100

    changed sidebar.blade.php to have the correct css styling

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,11 +1,10 @@
 <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 
-<aside class="sidebar">
+<aside class="sidebar home-layout">
     <h2 class="sidebar-title">Browse Lists</h2>
 
     <nav class="sidebar-nav">
-        <a href="#">All Lists</a>
-        <a href="#">Popular</a>
+        <a href="{{ route('lists.index') }}">All Lists</a>
     </nav>
 </aside>
